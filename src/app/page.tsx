5ae6dac954f9df43c11e486b385c33a9c8aba9c5
DaviBarretoo <davibarretojkda@gmail.com>
2024-09-08 20:26:20 -0300

    all ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,49 @@
-
+import {Home as HomeIcon, Search, Library} from "lucide-react";
 
 export default function Home() {
   return (
-    <main><h1>yes</h1></main>
+<div className="h-screen flex flex-col">
+  <div className="flex flex-1">
+    <aside className="w-72 bg-zinc-950 p-6">
+    <div className="flex items-center gap-2 ">
+          <div className="w-3 h-3 bg-red-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-yellow-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+       
+        </div>
+      <nav className="space-y-5 mt-10">
+        
+        <a href="" className="flex items-center gap-2 text-sm font-semibold text-zinc-200 "> 
+            <HomeIcon/>
+          Home
+          </a>
+        <a href="" className="flex items-center gap-2 text-sm font-semibold text-zinc-200 ">
+          <Search/> 
+          Search
+          </a>
+        <a href="" className="flex items-center gap-2 text-sm font-semibold text-zinc-200 ">
+          <Library/>
+          Your Library
+          </a>
+      </nav>
+
+      <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-2 ">
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100 ">Hino ❤ </a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100 ">Louvores do ❤ </a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100 ">Deus é perfeito </a>
+        <a href="" className="text-sm text-zinc-400 hover:text-zinc-100 ">This is Fernandinho </a>
+    
+
+      </nav>
+
+    </aside>
+    <main className="flex-1 p-6">
+      main
+    </main>
+  </div>
+  <footer className=" bg-zinc-800 border-t border-zinc-700 p-6">
+    footer
+  </footer>
+</div>
   );
 }
