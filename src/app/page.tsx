f380d886549f252e274d1b124ba0add7e243faaa
DaviBarretoo <davibarretojkda@gmail.com>
2024-09-08 23:17:40 -0300

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,27 +86,51 @@ export default function Home() {
       <h2 className="font-semibold text-2xl mt-10">Made for Davi Barreto</h2>
 
       <div className="grid grid-cols-8 gap-4 mt-4"> 
-<div className="bg-white/5  p-2  rounded">
+<a href="" className="bg-white/5  p-3   rounded-md flex flex-col gap-2 hover:bg-white/10">
+<Image src="/Colosssenses.jpg" className="w-full " width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
+<strong className="font-semibold">Colossenses</strong>
+<span className="text-sm text-zinc-400">Marcos Teles</span>
+
+</a>
+<a href="" className="bg-white/5  p-3   rounded-md flex flex-col gap-2 hover:bg-white/10">
+<Image src="/Colosssenses.jpg" className="w-full " width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
+<strong>Colossenses</strong>
+<span className="text-sm text-zinc-400">Marcos Teles</span>
+</a>
+<a href="" className="bg-white/5  p-3   rounded-md flex flex-col gap-2 hover:bg-white/10">
+<Image src="/Colosssenses.jpg" className="w-full " width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
+<strong>Colossenses</strong>
+<span className="text-sm text-zinc-400">Marcos Teles</span>
+</a>
+<a href="" className="bg-white/5  p-3   rounded-md flex flex-col gap-2 hover:bg-white/10">
+<Image src="/Colosssenses.jpg" className="w-full " width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
+<strong>Colossenses</strong>
+<span className="text-sm text-zinc-400">Marcos Teles</span>
+</a>
+<a  href="" className="bg-white/5  p-3   rounded-md flex flex-col gap-2 hover:bg-white/10">
 <Image src="/Colosssenses.jpg" className="w-full" width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
-</div>
-<div className="bg-white/5  p-2  rounded">
-<Image src="/Colosssenses.jpg" className="w-full" width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
-</div>
-<div className="bg-white/5  p-2  rounded">
-<Image src="/Colosssenses.jpg" className="w-full" width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
-</div>
-<div className="bg-white/5  p-2  rounded">
-<Image src="/Colosssenses.jpg" className="w-full" width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
-</div>
-<div className="bg-white/5  p-2  rounded">
-<Image src="/Colosssenses.jpg" className="w-full" width={1920} height={1080} alt="Colossenses é suas linhas de amor" />
-</div>
+<strong>Colossenses</strong>
+<span className="text-sm text-zinc-400">Marcos Teles</span>
+</a>
 
       </div>
     </main>
   </div>
-  <footer className=" bg-zinc-800 border-t border-zinc-700 p-6">
-    footer
+  <footer className=" bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+   <div className="flex items-center gap-2">
+   <Image src="/Colosssenses.jpg"  width={48} height={48} alt="Colossenses é suas linhas de amor" />
+ 
+   <div className="flex flex-col ">
+    <strong className="font-normal">Colossenses é suas linhas de amor</strong>
+    <span className="text-xs text-zinc-500">Marcos theles</span>
+   </div>
+   </div>
+   <div>
+
+   </div>
+   <div>
+
+   </div>
   </footer>
 </div>
   );
